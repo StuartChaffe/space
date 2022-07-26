f0a304bbda6c4f5aa8a27ef13e38d313a69d03e6
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-07-26 14:43:42 +0100

    Update post tpes

--- wp-content/plugins/site-functionality/post-types.php
+++ wp-content/plugins/site-functionality/post-types.php
@@ -23,153 +23,153 @@ function cp_change_post_object() {
 add_action('init', 'origin_cpts');
 
 function origin_cpts() {
-	$post_type_name = "case-study";
-	$single_name    = "Case Study";
-	$plural_name    = "Case studies";
-	$icon           = "dashicons-format-aside";
-	$gutenberg_enabled	= true;
+	// $post_type_name = "case-study";
+	// $single_name    = "Case Study";
+	// $plural_name    = "Case studies";
+	// $icon           = "dashicons-format-aside";
+	// $gutenberg_enabled	= true;
 
-	$post_type_options = array(
-	    'label'                 => $single_name,
-	    'public'                => true,
-	    'menu_icon'             => $icon,
-	    'show_ui'               => true,
-	    'show_in_menu'          => true,
-	    'capability_type'       => 'post',
-	    'hierarchical'          => true,
-	    'rewrite'               => array(
-	        'slug'                  => 'case-studies',
-	        'with_front'            => '0'
-	    ),
-	    'query_var'             => true,
-	    'has_archive'           => false,
-		'show_in_rest'			=> $gutenberg_enabled,
-	    'supports'              => array(
-	        'title',
-	        'editor',
-			'excerpt',
-	        'revisions',
-	        'thumbnail',
-	        'author',
-	        'page-attributes',
-	    ),
-	    'labels'                => array(
-	        'name'                  => $plural_name,
-	        'singular_name'         => $single_name,
-	        'menu_name'             => $plural_name,
-	        'add_new'               => 'Add ' . $single_name,
-	        'add_new_item'          => 'Add New ' . $single_name,
-	        'edit'                  => 'Edit',
-	        'edit_item'             => 'Edit ' . $single_name,
-	        'new_item'              => 'New '. $single_name,
-	        'view'                  => 'View '. $single_name,
-	        'view_item'             => 'View '. $single_name,
-	        'search_items'          => 'Search ' . $plural_name,
-	        'not_found'             => 'No '. $plural_name . ' Found',
-	        'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
-	        'parent'                => 'Parent '. $single_name
-	    ),
-	);
-	register_post_type($post_type_name, $post_type_options);
+	// $post_type_options = array(
+	//     'label'                 => $single_name,
+	//     'public'                => true,
+	//     'menu_icon'             => $icon,
+	//     'show_ui'               => true,
+	//     'show_in_menu'          => true,
+	//     'capability_type'       => 'post',
+	//     'hierarchical'          => true,
+	//     'rewrite'               => array(
+	//         'slug'                  => 'case-studies',
+	//         'with_front'            => '0'
+	//     ),
+	//     'query_var'             => true,
+	//     'has_archive'           => false,
+	// 	'show_in_rest'			=> $gutenberg_enabled,
+	//     'supports'              => array(
+	//         'title',
+	//         'editor',
+	// 		'excerpt',
+	//         'revisions',
+	//         'thumbnail',
+	//         'author',
+	//         'page-attributes',
+	//     ),
+	//     'labels'                => array(
+	//         'name'                  => $plural_name,
+	//         'singular_name'         => $single_name,
+	//         'menu_name'             => $plural_name,
+	//         'add_new'               => 'Add ' . $single_name,
+	//         'add_new_item'          => 'Add New ' . $single_name,
+	//         'edit'                  => 'Edit',
+	//         'edit_item'             => 'Edit ' . $single_name,
+	//         'new_item'              => 'New '. $single_name,
+	//         'view'                  => 'View '. $single_name,
+	//         'view_item'             => 'View '. $single_name,
+	//         'search_items'          => 'Search ' . $plural_name,
+	//         'not_found'             => 'No '. $plural_name . ' Found',
+	//         'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
+	//         'parent'                => 'Parent '. $single_name
+	//     ),
+	// );
+	// register_post_type($post_type_name, $post_type_options);
 
-	$post_type_name		= "careers";
-	$single_name		= "Job";
-	$plural_name		= "Careers";
-	$icon				= "dashicons-groups";
-	$gutenberg_enabled	= true;
+	// $post_type_name		= "careers";
+	// $single_name		= "Job";
+	// $plural_name		= "Careers";
+	// $icon				= "dashicons-groups";
+	// $gutenberg_enabled	= true;
 	
-	$post_type_options = array(
-	    'label'                 => $single_name,
-	    'public'                => true,
-	    'menu_icon'             => $icon,
-	    'show_ui'               => true,
-	    'show_in_menu'          => true,
-	    'capability_type'       => 'post',
-	    'hierarchical'          => true,
-	    'rewrite'               => array(
-	        'slug'                  => 'careers',
-	        'with_front'            => '0'
-	    ),
-	    'query_var'             => true,
-	    'has_archive'           => false,
-		'show_in_rest'			=> $gutenberg_enabled,
-	    'supports'              => array(
-	        'title',
-	        'editor',
-			'excerpt',
-	        'revisions',
-	        'thumbnail',
-	        'author',
-	        'page-attributes',
-	    ),
-	    'labels'                => array(
-	        'name'                  => $plural_name,
-	        'singular_name'         => $single_name,
-	        'menu_name'             => $plural_name,
-	        'add_new'               => 'Add ' . $single_name,
-	        'add_new_item'          => 'Add New ' . $single_name,
-	        'edit'                  => 'Edit',
-	        'edit_item'             => 'Edit ' . $single_name,
-	        'new_item'              => 'New '. $single_name,
-	        'view'                  => 'View '. $single_name,
-	        'view_item'             => 'View '. $single_name,
-	        'search_items'          => 'Search ' . $plural_name,
-	        'not_found'             => 'No '. $plural_name . ' Found',
-	        'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
-	        'parent'                => 'Parent '. $single_name
-	    ),
-	);
-	register_post_type($post_type_name, $post_type_options);
+	// $post_type_options = array(
+	//     'label'                 => $single_name,
+	//     'public'                => true,
+	//     'menu_icon'             => $icon,
+	//     'show_ui'               => true,
+	//     'show_in_menu'          => true,
+	//     'capability_type'       => 'post',
+	//     'hierarchical'          => true,
+	//     'rewrite'               => array(
+	//         'slug'                  => 'careers',
+	//         'with_front'            => '0'
+	//     ),
+	//     'query_var'             => true,
+	//     'has_archive'           => false,
+	// 	'show_in_rest'			=> $gutenberg_enabled,
+	//     'supports'              => array(
+	//         'title',
+	//         'editor',
+	// 		'excerpt',
+	//         'revisions',
+	//         'thumbnail',
+	//         'author',
+	//         'page-attributes',
+	//     ),
+	//     'labels'                => array(
+	//         'name'                  => $plural_name,
+	//         'singular_name'         => $single_name,
+	//         'menu_name'             => $plural_name,
+	//         'add_new'               => 'Add ' . $single_name,
+	//         'add_new_item'          => 'Add New ' . $single_name,
+	//         'edit'                  => 'Edit',
+	//         'edit_item'             => 'Edit ' . $single_name,
+	//         'new_item'              => 'New '. $single_name,
+	//         'view'                  => 'View '. $single_name,
+	//         'view_item'             => 'View '. $single_name,
+	//         'search_items'          => 'Search ' . $plural_name,
+	//         'not_found'             => 'No '. $plural_name . ' Found',
+	//         'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
+	//         'parent'                => 'Parent '. $single_name
+	//     ),
+	// );
+	// register_post_type($post_type_name, $post_type_options);
 
 
 
-	$post_type_name		= "team";
-	$single_name		= "Team Member";
-	$plural_name		= "Team";
-	$icon				= "dashicons-admin-users";
-	$gutenberg_enabled	= false;
+	// $post_type_name		= "team";
+	// $single_name		= "Team Member";
+	// $plural_name		= "Team";
+	// $icon				= "dashicons-admin-users";
+	// $gutenberg_enabled	= false;
 
-	$post_type_options = array(
-		'label'                 => $single_name,
-		'public'                => false,
-		'menu_icon'             => $icon,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'capability_type'       => 'post',
-		'hierarchical'          => true,
-		'rewrite'               => array(
-			'slug'                  => '',
-			'with_front'            => '0'
-		),
-		'query_var'             => true,
-		'has_archive'           => false,
-		'show_in_rest'			=> $gutenberg_enabled,
-		'supports'              => array(
-			'title',
-			'editor',
-			'revisions',
-			'thumbnail',
-			'author',
-			'page-attributes',
-		),
-		'labels'                => array(
-			'name'                  => $plural_name,
-			'singular_name'         => $single_name,
-			'menu_name'             => $plural_name,
-			'add_new'               => 'Add ' . $single_name,
-			'add_new_item'          => 'Add new ' . $single_name,
-			'edit'                  => 'Edit',
-			'edit_item'             => 'Edit ' . $single_name,
-			'new_item'              => 'New '. $single_name,
-			'view'                  => 'View '. $single_name,
-			'view_item'             => 'View '. $single_name,
-			'search_items'          => 'Search ' . $plural_name,
-			'not_found'             => 'No '. $plural_name . ' Found',
-			'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
-			'parent'                => 'Parent '. $single_name
-		),
-	);
-	register_post_type($post_type_name, $post_type_options);
+	// $post_type_options = array(
+	// 	'label'                 => $single_name,
+	// 	'public'                => false,
+	// 	'menu_icon'             => $icon,
+	// 	'show_ui'               => true,
+	// 	'show_in_menu'          => true,
+	// 	'capability_type'       => 'post',
+	// 	'hierarchical'          => true,
+	// 	'rewrite'               => array(
+	// 		'slug'                  => '',
+	// 		'with_front'            => '0'
+	// 	),
+	// 	'query_var'             => true,
+	// 	'has_archive'           => false,
+	// 	'show_in_rest'			=> $gutenberg_enabled,
+	// 	'supports'              => array(
+	// 		'title',
+	// 		'editor',
+	// 		'revisions',
+	// 		'thumbnail',
+	// 		'author',
+	// 		'page-attributes',
+	// 	),
+	// 	'labels'                => array(
+	// 		'name'                  => $plural_name,
+	// 		'singular_name'         => $single_name,
+	// 		'menu_name'             => $plural_name,
+	// 		'add_new'               => 'Add ' . $single_name,
+	// 		'add_new_item'          => 'Add new ' . $single_name,
+	// 		'edit'                  => 'Edit',
+	// 		'edit_item'             => 'Edit ' . $single_name,
+	// 		'new_item'              => 'New '. $single_name,
+	// 		'view'                  => 'View '. $single_name,
+	// 		'view_item'             => 'View '. $single_name,
+	// 		'search_items'          => 'Search ' . $plural_name,
+	// 		'not_found'             => 'No '. $plural_name . ' Found',
+	// 		'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
+	// 		'parent'                => 'Parent '. $single_name
+	// 	),
+	// );
+	// register_post_type($post_type_name, $post_type_options);
 
 	$post_type_name		= "testimonials";
 	$single_name		= "Testimonial";
