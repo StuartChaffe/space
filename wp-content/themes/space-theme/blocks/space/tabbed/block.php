13bdd50583b2eca6b8bb2ccf0e029661df01a7cf
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-08-03 15:56:29 +0100

    Add ID to block

--- wp-content/themes/space-theme/blocks/space/tabbed/block.php
+++ wp-content/themes/space-theme/blocks/space/tabbed/block.php
@@ -13,13 +13,14 @@ $tab1	= get_field('tab_1');
 $tab2	= get_field('tab_2');
 $tab3	= get_field('tab_3');
 ?>
+
 <section class="tabbed sm--bottom">
 	<div class="tabbed--buttons" role="tablist" aria-label="TabCordion">
-		<button class="tab is-active" role="tab" aria-selected="true" aria-controls="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab1-tab" id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab1"><?php echo esc_html( $tab1['tab_title'] ); ?></button>
-		<button class="tab" role="tab" aria-selected="false" aria-controls="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab2-tab" id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab2" tabindex="-1"><?php echo esc_html( $tab2['tab_title'] ); ?></button>
-		<button class="tab" role="tab" aria-selected="false" aria-controls="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab3-tab" id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab3" tabindex="-1"><?php echo esc_html( $tab3['tab_title'] ); ?></button>
+		<button class="tab is-active" role="tab" aria-selected="true" aria-controls="<?php echo $block['id']; ?>-tab1-tab" id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab1"><?php echo esc_html( $tab1['tab_title'] ); ?></button>
+		<button class="tab" role="tab" aria-selected="false" aria-controls="<?php echo $block['id']; ?>-tab2-tab" id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab2" tabindex="-1"><?php echo esc_html( $tab2['tab_title'] ); ?></button>
+		<button class="tab" role="tab" aria-selected="false" aria-controls="<?php echo $block['id']; ?>-tab3-tab" id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab3" tabindex="-1"><?php echo esc_html( $tab3['tab_title'] ); ?></button>
 	</div>
-	<div id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab1-tab" class="tabbed--item is-active" data-title="<?php echo $tab1['tab_title']; ?>" tabindex="0" role="tabpanel" aria-labelledby="tab1">
+	<div id="<?php echo $block['id']; ?>-tab1-tab" class="tabbed--item is-active" data-title="<?php echo $tab1['tab_title']; ?>" tabindex="0" role="tabpanel" aria-labelledby="tab1">
 		<div class="tabbed--item-container">
 			<div class="tabbed--item__inner">
 				<div class="tabbed--item__content content">
@@ -33,7 +34,7 @@ $tab3	= get_field('tab_3');
 			</div>
 		</div>
 	</div>
-	<div id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab2-tab" class="tabbed--item" data-title="<?php echo $tab2['tab_title']; ?>" tabindex="-1" role="tabpanel" aria-labelledby="tab2">
+	<div id="<?php echo $block['id']; ?>-tab2-tab" class="tabbed--item" data-title="<?php echo $tab2['tab_title']; ?>" tabindex="-1" role="tabpanel" aria-labelledby="tab2">
 		<div class="tabbed--item-container">
 			<div class="tabbed--item__inner">
 				<div class="tabbed--item__content content">
@@ -46,7 +47,7 @@ $tab3	= get_field('tab_3');
 			</div>
 		</div>
 	</div>
-	<div id="<?php if ( $id ) { ?><?php echo $id ?>-<?php } ?>tab3-tab" class="tabbed--item" data-title="<?php echo $tab3['tab_title']; ?>" tabindex="-1" role="tabpanel" aria-labelledby="tab3">
+	<div id="<?php echo $block['id']; ?>-tab3-tab" class="tabbed--item" data-title="<?php echo $tab3['tab_title']; ?>" tabindex="-1" role="tabpanel" aria-labelledby="tab3">
 		<div class="tabbed--item-container">
 			<div class="tabbed--item__inner">
 				<div class="tabbed--item__content content">
